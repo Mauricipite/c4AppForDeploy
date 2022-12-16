d0bb0afc1e23d3e8a5c330ad72941c7cfd8c2527
Mauricipite <mauricipite@gmail.com>
2022-12-16 13:18:02 -0500

    Couldn't figure it out, I used MELD to check for differences, there were some, but the project didn't work even after I fixed them. Had to copy the whole front end folder from working-cloned-project keeping my register page to make it work, and it does now, this commit is the final one before implementing my alterations

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,171 +1,171 @@
-import {useState, useEffect} from 'react'
-import {FaUser} from 'react-icons/fa'
-import {useSelector, useDispatch} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
-import {toast} from 'react-toastify'
-import {register, reset} from '../features/auth/authSlice'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { FaUser } from 'react-icons/fa'
+import { register, reset } from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
 
 function Register() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        password2: '',
-        phoneNumber: '',
-        identification: '',
-        address: ''
-    })
+  const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      phoneNumber: '',
+      identification: '',
+      address: ''
+  })
 
-    const {name, email, password, password2, phoneNumber, identification, address} = formData
+  const {name, email, password, password2, phoneNumber, identification, address} = formData
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+  const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
 
-    useEffect(() => {
-        if(isError) {
-            toast.error(message)
-        }
+  useEffect(() => {
+      if(isError) {
+          toast.error(message)
+      }
 
-        if(isSuccess || user) {
-            navigate('/')
-        }
+      if(isSuccess || user) {
+          navigate('/')
+      }
 
-        dispatch(reset())
-    }, [user, isError, isSuccess, message, navigate, dispatch])
-    
-    const onChange = (e) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }))
-    }
+      dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+  
+  const onChange = (e) => {
+      setFormData((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value,
+      }))
+  }
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+  const onSubmit = (e) => {
+      e.preventDefault()
 
-        if(password !== password2) {
-            toast.error('Passwords do not match')
-        } else {
-            const userData = {
-                name,
-                email,
-                phoneNumber,
-                identification,
-                address,
-                password
-            }
+      if(password !== password2) {
+          toast.error('Passwords do not match')
+      } else {
+          const userData = {
+              name,
+              email,
+              phoneNumber,
+              identification,
+              address,
+              password
+          }
 
-            dispatch(register(userData))
-        }
-    }
+          dispatch(register(userData))
+      }
+  }
 
-    if(isLoading) {
-        return <Spinner/>
-    }
+  if(isLoading) {
+      return <Spinner/>
+  }
 
-    return <>
-        <section className='heading'>
-            <h1>
-                <FaUser/> Register
-            </h1>
-            <p>Please create an account</p>
-        </section>
+  return <>
+      <section className='heading'>
+          <h1>
+              <FaUser/> Register
+          </h1>
+          <p>Please create an account</p>
+      </section>
 
-        <section className='form'>
-            <form onSubmit={onSubmit}>
-                {/*name input*/}
-                <div className="form-group">
-                    <input
-                        type='text'
-                        className='form-control'
-                        id='name'
-                        name='name'
-                        value={name}
-                        placeholder='Enter your name'
-                        onChange={onChange}
-                    />
-                </div>
-                {/*email input*/}
-                <div className="form-group">
-                    <input
-                        type='email'
-                        className='form-control'
-                        id='email'
-                        name='email'
-                        value={email}
-                        placeholder='Enter your email'
-                        onChange={onChange}
-                    />
-                </div>
-                {/*password input*/}
-                <div className="form-group">
-                    <input
-                        type='password'
-                        className='form-control'
-                        id='password'
-                        name='password'
-                        value={password}
-                        placeholder='Enter a password'
-                        onChange={onChange}
-                    />
-                </div>
-                {/*confirm password input*/}
-                <div className="form-group">
-                    <input
-                        type='password'
-                        className='form-control'
-                        id='password2'
-                        name='password2'
-                        value={password2}
-                        placeholder='Confirm password'
-                        onChange={onChange}
-                    />
-                </div>
-                {/*phoneNumber input*/}
-                <div className="form-group">
-                    <input
-                        type='tel'
-                        className='form-control'
-                        id='phoneNumber'
-                        name='phoneNumber'
-                        value={phoneNumber}
-                        placeholder='Enter your phone Number'
-                        onChange={onChange}
-                    />
-                </div>
-                {/*identification input*/}
-                <div className="form-group">
-                    <input
-                        type='number'
-                        className='form-control'
-                        id='identification'
-                        name='identification'
-                        value={identification}
-                        placeholder='Enter your ID/identification'
-                        onChange={onChange}
-                    />
-                </div>
-                {/*address input*/}
-                <div className="form-group">
-                    <input
-                        type='text'
-                        className='form-control'
-                        id='address'
-                        name='address'
-                        value={address}
-                        placeholder='Enter your address'
-                        onChange={onChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <button type="submit" className='btn btn-block'>Submit</button>
-                </div>
-            </form>
-        </section>
-    </>
+      <section className='form'>
+          <form onSubmit={onSubmit}>
+              {/*name input*/}
+              <div className="form-group">
+                  <input
+                      type='text'
+                      className='form-control'
+                      id='name'
+                      name='name'
+                      value={name}
+                      placeholder='Enter your name'
+                      onChange={onChange}
+                  />
+              </div>
+              {/*email input*/}
+              <div className="form-group">
+                  <input
+                      type='email'
+                      className='form-control'
+                      id='email'
+                      name='email'
+                      value={email}
+                      placeholder='Enter your email'
+                      onChange={onChange}
+                  />
+              </div>
+              {/*password input*/}
+              <div className="form-group">
+                  <input
+                      type='password'
+                      className='form-control'
+                      id='password'
+                      name='password'
+                      value={password}
+                      placeholder='Enter a password'
+                      onChange={onChange}
+                  />
+              </div>
+              {/*confirm password input*/}
+              <div className="form-group">
+                  <input
+                      type='password'
+                      className='form-control'
+                      id='password2'
+                      name='password2'
+                      value={password2}
+                      placeholder='Confirm password'
+                      onChange={onChange}
+                  />
+              </div>
+              {/*phoneNumber input*/}
+              <div className="form-group">
+                  <input
+                      type='tel'
+                      className='form-control'
+                      id='phoneNumber'
+                      name='phoneNumber'
+                      value={phoneNumber}
+                      placeholder='Enter your phone Number'
+                      onChange={onChange}
+                  />
+              </div>
+              {/*identification input*/}
+              <div className="form-group">
+                  <input
+                      type='number'
+                      className='form-control'
+                      id='identification'
+                      name='identification'
+                      value={identification}
+                      placeholder='Enter your ID/identification'
+                      onChange={onChange}
+                  />
+              </div>
+              {/*address input*/}
+              <div className="form-group">
+                  <input
+                      type='text'
+                      className='form-control'
+                      id='address'
+                      name='address'
+                      value={address}
+                      placeholder='Enter your address'
+                      onChange={onChange}
+                  />
+              </div>
+              <div className="form-group">
+                  <button type="submit" className='btn btn-block'>Submit</button>
+              </div>
+          </form>
+      </section>
+  </>
 }
 
 export default Register
