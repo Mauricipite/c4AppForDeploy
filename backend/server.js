5afb1900ce774ef4ee23c72640aee7991cf81ab3
Mauricipite <mauricipite@gmail.com>
2023-01-02 06:07:54 -0500

    i've only edited the server on backend, will make the other changes to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
@@ -16,6 +17,15 @@ app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/computers', require('./routes/computerRoutes'))
 app.use('/api/orders', require('./routes/orderRoutes'))
+
+//serve frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production or something like that'))
+}
 
 app.use(errorHandler)
 
