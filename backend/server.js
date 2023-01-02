6714956c010107472268d729477277955c72f324
Mauricipite <mauricipite@gmail.com>
2023-01-02 06:55:27 -0500

    returned server file on backend to 'stock', let's see if it works

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const path = require('path')
+// const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
@@ -18,14 +18,14 @@ app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/computers', require('./routes/computerRoutes'))
 app.use('/api/orders', require('./routes/orderRoutes'))
 
-//serve frontend
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+// //serve frontend
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-} else {
-    app.get('/', (req, res) => res.send('Please set to production or something like that'))
-}
+//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+// } else {
+//     app.get('/', (req, res) => res.send('Please set to production or something like that'))
+// }
 
 app.use(errorHandler)
 
